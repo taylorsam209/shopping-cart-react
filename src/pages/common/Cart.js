2534202f1ad956e0bd1dfef8049fd582afb74c21
Taylor Sam <taylorsam209@yahoo.com>
2018-09-13 22:17:54 -0700

    Added totalcalculation method for cart

--- src/pages/common/Cart.js
+++ src/pages/common/Cart.js
@@ -1,47 +1,81 @@
-import React from 'react';
-import {connect} from 'react-redux';
-import {addToCart, removeFromCart, removeAllFromCart} from '../../ducks/CartReducer';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  addToCart,
+  removeFromCart,
+  removeAllFromCart
+} from "../../ducks/CartReducer";
 
-function sortByProductId(products) {
-    return products.sort((a,b) => a.id < b.id)
-}
+class Cart extends Component {
+  state = {
+    total: 0
+  };
+  componentWillMount() {
+    this.calculateTotal();
+    this.sortByProductId();
+  }
 
-function Cart(props) {
-    return props.cart.length ?
-    <table>
-        <thead>
-            <tr>
-                <th>Item</th>
-                <th>Quantity</th>
-                <th></th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            {sortByProductId(props.cart).map(item => {
-                return (
-                    <tr>
-                        <td>{item.name}</td>
-                        <td>{item.quantity}</td>
-
-                    <td>
-                        <button onClick={(e) => props.addToCart(item)}>+</button>
-                        <button onClick={(e) => props.removeFromCart(item)}>-</button>
-                    </td>
-                    <td>
-                        <button onClick={(e) => props.removeAllFromCart(item)}>Remove all from cart</button>
-                    </td>
-                    </tr>
-                )
-            })}
-        </tbody>
-    </table> : null
-
-}
-
-function mapStateToProps(state){
-    return {
-        cart: state.cartReducer.cart
+  calculateTotal() {
+    if (this.props.cart.length) {
+      let total = 0;
+      this.props.cart.map(product => (total = total + product.price * product.quantity));
+      this.setState({ total: total.toFixed(2) });
     }
+  }
+
+  //Sorts all product by ID. This should be done from the database prior possibly, adding cart button should push directly to database
+  sortByProductId() {
+    return this.props.cart.sort((a, b) => a.id < b.id);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.cart !== prevProps.cart) {
+      this.calculateTotal();
+      this.sortByProductId();
+    }
+  }
+
+  render() {
+    console.log(this.props.cart);
+    return this.props.cart.length ? (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Item</th>
+              <th>Quantity</th>
+              <th />
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.cart.map(item => {
+              return (
+                <tr>
+                  <td>{item.name}</td>
+                  <td>{item.quantity}</td>
+                  <td>
+                    <button onClick={e => this.props.addToCart(item)}>+</button>
+                    <button onClick={e => this.props.removeFromCart(item)}>-</button>
+                  </td>
+                  <td>
+                    <button onClick={e => this.props.removeAllFromCart(item)}>Remove all from cart</button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <h1>Total: {this.state.total}</h1>
+      </div>
+    ) : null;
+  }
 }
-export default connect(mapStateToProps, {addToCart, removeFromCart, removeAllFromCart})(Cart);
+
+function mapStateToProps(state) {
+  return {
+    cart: state.cartReducer.cart
+  };
+}
+
+export default connect(mapStateToProps, { addToCart, removeFromCart, removeAllFromCart })(Cart);
